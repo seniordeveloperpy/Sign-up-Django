2029790babb21ab3e5e363407a45a753d46e3cd5
seniordeveloperpy <kamronbekmubinaxon18@gmail.com>
2024-04-06 23:07:47 +0500

    first commit

---
+++ mainapp/urls.py
@@ -0,0 +1,14 @@
+# from unicodedata import name
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # password change
+    path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+]
